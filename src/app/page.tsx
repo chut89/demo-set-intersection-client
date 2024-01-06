f34d078e9712fa9e657259503d83d6d3c74934df
chut89 <chutienha_1989@yahoo.com>
2024-01-06 01:43:28 +0100

    add clock example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { useState, useEffect } from 'react';
+
 function getImageUrl(imageId, size = 's') {
   return ('https://i.imgur.com/' +
     imageId +
@@ -58,7 +61,7 @@ function Card({ title, content }) {
     );
 }
 
-export default function Gallery() {
+function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
@@ -96,6 +99,38 @@ export default function Gallery() {
           />  
       </section>
     </div>
+  );
+}
+
+function Clock({ time }) {
+  function calculateDayNight() {
+    let hours = time.getHours();
+    return hours >= 0 && hours <= 6 ? 'night' : 'day';
+  }
+
+  return (
+    <h1 id="time" className={calculateDayNight(time)}>
+      {time.toLocaleTimeString()}
+    </h1>
+  );
+}
+
+function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time;
+}
+
+export default function App() {
+  const time = useTime();
+  //let hours = time.getHours();
+  return (
+    <Clock time={time} />
   );
 }
 
