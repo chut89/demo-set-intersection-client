e47105d3f207716bf9b52271871a3bfaafb91ffc
chut89 <chutienha_1989@yahoo.com>
2024-01-10 00:08:50 +0100

    add input route, lift state up one level

---
+++ src/app/input/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { useState } from 'react';
+import './input.css';
+
+export default function SyncedInputs() {
+  const [text, setText] = useState('');
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <>
+      <Input label="First input" text={text} handleChange={handleChange} />
+      <Input label="Second input" text={text} handleChange={handleChange} />
+    </>
+  );
+}
+
+function Input({ label, text, handleChange }) {
+  return (
+    <label>
+      {label}
+      {' '}
+      <input
+        value={text}
+        onChange={handleChange}
+      />
+    </label>
+  );
+}
