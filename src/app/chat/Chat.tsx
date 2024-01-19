48fc520366936447222be8ffa6eea9fcede95e37
chut89 <chutienha_1989@yahoo.com>
2024-01-19 20:45:49 +0100

    chat example: add undo button

--- src/app/chat/Chat.tsx
+++ src/app/chat/Chat.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Chat({contact, message, dispatch}) {
+  const [isSending, setIsSending] = useState(false);
+  const timeoutRef = useRef(null);
+
   return (
     <section className="chat">
       <textarea
+        disabled={isSending}
         value={message}
         placeholder={'Chat to ' + contact.name}
         onChange={(e) => {
@@ -16,11 +20,24 @@ export default function Chat({contact, message, dispatch}) {
       />
       <br />
       <button
+        disabled={isSending}
         onClick={() => {
-          alert(message + ' was sent to ' + contact.name);
-          dispatch({ type: 'edited_message', contactId: contact.id, message: '' });
+          setIsSending(true); 
+          timeoutRef.current = setTimeout(() => {
+            alert(message + ' was sent to ' + contact.name);
+            dispatch({ type: 'edited_message', contactId: contact.id, message: '' });
+            setIsSending(false);
+          }, 3000);
         }}
       >Send to {contact.email}</button>
+      {isSending &&
+        <button onClick={() => {
+          setIsSending(false);
+          clearTimeout(timeoutRef.current);                    
+        }}>
+          Undo
+        </button>
+      }
     </section>
   );
 }
